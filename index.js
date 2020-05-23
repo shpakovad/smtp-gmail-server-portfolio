a7662272487047a5f3989cc5d935ff099e7f20d7
Dasha <shpakovad@gmail.com>
2020-05-23 15:01:56 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const express = require('express');
+const nodemailer = require("nodemailer");
+const cors = require("cors");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.use(cors())
+app.use(bodyParser.urlencoded({extended:false}))
+app.use(bodyParser.json())
+
+// let smtp_login = process.env.SMTP_LOGIN || "---"
+// let smtp_password = process.env.SMTP_PASSWORD || "---"
+
+let transporter = nodemailer.createTransport({
+    service:"gmail",
+    auth: {
+        // user: smtp_login ,
+        // pass: smtp_password,
+        user: "shpakovad@gmail.com" ,
+        pass: "80291332819",
+    },
+});
+
+app.get("/" ,function(req,res){
+    res.send("All is work!")
+})
+
+app.post("/sendMessage", async function (req, res) {
+
+    let {name,email,message} = req.body
+
+    let info = await transporter.sendMail({
+        from: "Portfolio Page",
+        to: "shpakovad@gmail.com",
+        subject: "Message from Portfolio Page",
+        html: `<b>Message from Portfolio Page</b>,
+<div>name: ${name}</div>
+<div>email: ${email}</div>
+<div>message: ${message}</div>
+`
+    });
+    res.send('Message sent');
+});
+
+// let port = process.env.PORT || 3010
+// app.listen(port, function () {
+//     console.log('App listening on port 3010!');
+// });
+
+
+app.listen(3010, function () {
+    console.log('App listening on port 3010!');
+});
