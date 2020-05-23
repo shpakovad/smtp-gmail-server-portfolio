913de68281733daacb5aa4252c51dffe50e0e1ee
Dasha <shpakovad@gmail.com>
2020-05-23 19:56:15 +0300

    #3

--- index.js
+++ index.js
@@ -5,12 +5,27 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors({
-    credentials: 'true',
-    origin: '*',
-    methods: 'GET, POST, PUT, DELETE, OPTIONS',
-    allowedHeaders: 'Authorization, Access-Control-Allow-Headers, Origin,X-Requested-With,Content-Type,Accept,content-type,application/json'
-}))
+var allowCrossDomain = function(req, res, next) {
+    res.header('Access-Control-Allow-Origin', '*');
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE');
+    res.header('Access-Control-Allow-Headers', 'X-Requested-With, Accept, Origin, Referer, User-Agent, Content-Type, Authorization');
+
+    // intercept OPTIONS method
+    if (req.method === 'OPTIONS') {
+        res.send(200);
+    }
+    else {
+        next();
+    }
+};
+
+// you might have "app" instead of "server"
+app.configure(function() {
+    server.use(allowCrossDomain);   // make sure this is is called before the router
+    server.use(server.router);      // not entirely necessary--will be automatically called with the first .get()
+});
+
+app.use(cors())
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(bodyParser.json())
 
