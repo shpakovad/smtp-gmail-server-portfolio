da7524380a65811a265b06a13a39b6e2bd254760
Dasha <shpakovad@gmail.com>
2020-05-25 14:06:23 +0300

    #11

--- index.js
+++ index.js
@@ -5,11 +5,7 @@ const bodyParser = require("body-parser");
 
 const app = express();
 
-app.use(cors({
-        origin: '*',
-        optionsSuccessStatus: 200 // some legacy browsers (IE11, various SmartTVs) choke on 204
-    }
-))
+app.use(cors())
 app.use(bodyParser.urlencoded({extended: true}))
 app.use(bodyParser.json())
 
